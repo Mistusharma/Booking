5948efe8304dd85f93bdeda606b4c30298f0d5ca
Mistusharma <117651010+Mistusharma@users.noreply.github.com>
2024-11-21 09:19:29 +0530

    Update auth.js

--- Booking/node-booking/routes/auth.js
+++ Booking/node-booking/routes/auth.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
-const { signService, loginService } = require('../service/auth')
+const { signService, loginService, forgetPasswordService } = require('../service/auth')
 const auth = Router(); // Correct way to initialize Router
 
 // Define the route with the correct HTTP method and path
 auth.post('/signup', signService); // Use '/' for the route path
 auth.post('/login', loginService); // Use '/' for the route path
+auth.post('/forgetPassword', forgetPasswordService)
+auth.post('/resetPassword',resetPassword)
+
+
+
+
 
 module.exports = auth;
